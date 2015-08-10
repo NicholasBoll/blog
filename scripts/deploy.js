1b8c7f5aa706f77f1462eef2a49d105edb15413c
Nicholas Boll <nicholas.boll@gmail.com>
2015-08-09 21:12:25 -0600

    Updated deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,9 @@
 var shell = require('shelljs');
 
 shell.rm('-rf', 'build');
-shell.exec('git clone https://github.com/NicholasBoll/blog.git build');
-shell.exec('cd build; git checkout gh-pages');
+shell.exec('git clone https://github.com/NicholasBoll/nicholasboll.github.io.git build');
 shell.rm('-rf', ['build/articles', 'build/tag', 'build/css'])
-shell.exec('wintersmith build --baseUrl="/blog"');
+shell.exec('wintersmith build');
 shell.exec('cd build; git add -A');
 shell.exec('cd build; git commit -m "Auto-deploy"');
-shell.exec('cd build; git push origin gh-pages -f');
+shell.exec('cd build; git push origin master');
